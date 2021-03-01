4e31b942597acba4ef5d9480a714e159bfc646e0
Rafael Campos <rafacampos2000@hotmail.com>
2021-03-01 11:23:45 -0300

    final commit

---
+++ src/app.ts
@@ -0,0 +1,27 @@
+import "reflect-metadata"
+import express, { NextFunction } from 'express';
+import "express-async-errors"
+import createConnection from "./database"
+import { AppError } from './errors/AppError';
+import { router } from './routes';
+import {Request, Response} from 'express'
+const app =  express();
+
+createConnection();
+app.use(express.json());
+app.use(router);
+
+app.use((err: Error, request: Request , response: Response, _next: NextFunction) => {
+    if(err instanceof AppError){
+        return response.status(err.statusCode).json({
+            message : err.message
+        })
+    }
+
+    return response.status(500).json({
+        status: "Error",
+        message : `Internal server error ${err.message}`,
+    })
+})
+
+export { app}
